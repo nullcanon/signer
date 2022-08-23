f73025a169e0ff51eefbd0afa09a6759de3e20ee
ubuntu <ubuntu@localhost.localdomain>
2022-08-23 19:00:13 +0800

    add signer

--- go.mod
+++ go.mod
@@ -3,7 +3,11 @@ module signer
 go 1.16
 
 require (
+	github.com/aristanetworks/goarista v0.0.0-20220525153724-48e9df1e1e4f // indirect
+	github.com/btcsuite/btcd v0.22.1 // indirect
 	github.com/ethereum/go-ethereum v1.10.18
 	github.com/miguelmota/go-solidity-sha3 v0.1.1
 	github.com/naoina/toml v0.1.2-0.20170918210437-9fafd6967416
+	github.com/ubiq/go-ubiq v3.0.1+incompatible // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 )
--- singer_service.go
+++ singer_service.go
@@ -2,10 +2,21 @@ package signer
 
 import (
 	"encoding/hex"
+	"errors"
 	"log"
 
-	"github.com/ethereum/go-ethereum/crypto/secp256k1"
+	// "github.com/ubiq/go-ubiq/crypto"
+	// "github.com/ethereum/go-ethereum/common"
+	"fmt"
+	// "github.com/ethereum/go-ethereum/crypto/secp256k1"
 	solsha3 "github.com/miguelmota/go-solidity-sha3"
+	"github.com/ubiq/go-ubiq/crypto/secp256k1"
+
+	// "github.com/btcsuite/btcd/btcec"
+	crypto1 "github.com/ubiq/go-ubiq/crypto"
+	// "github.com/ethereum/go-ethereum/hexutil"
+	"github.com/ubiq/go-ubiq/common/hexutil"
+	"golang.org/x/crypto/sha3"
 )
 
 type RateParams struct {
@@ -30,9 +41,11 @@ func (s *SignerService) Sign(address string, amount string, nonce string) (strin
 	// 	panic(err)
 	// }
 	//0x2BB32bBE01840DEb3d2512C74c4fdA7c1992993D
+	// input pirvate key
 	pkeyb, err := hex.DecodeString("")
 	if err != nil {
-		s.logger.Fatalln(err)
+		// s.logger.Fatalln(err)
+		return "", err
 	}
 
 	// message := "TEST"
@@ -73,8 +86,107 @@ func (s *SignerService) Sign(address string, amount string, nonce string) (strin
 	// sig, err := secp256k1.Sign(prefixed, math.PaddedBigBytes(key.D, 32))
 	sig, err := secp256k1.Sign(prefixed, pkeyb)
 	if err != nil {
-		panic(err)
+		// panic(err)
+		return "", err
 	}
 	s.logger.Printf("Process sign : %s, %s, %s ,%s", address, amount, nonce, hex.EncodeToString(sig))
 	return hex.EncodeToString(sig), nil
+}
+
+func (s *SignerService) SignMessage(msg string, prv string) (string, error) {
+	// key, err := ecdsa.GenerateKey(crypto.S256(), rand.Reader)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	//0x2BB32bBE01840DEb3d2512C74c4fdA7c1992993D
+	pkeyb, err := hex.DecodeString(prv)
+	if err != nil {
+		// s.logger.Fatalln(err)
+		return "", err
+	}
+
+	types := []string{"bytes"}
+	values := []interface{}{
+		msg,
+	}
+	hash := solsha3.SoliditySHA3(types, values)
+
+	types = []string{"string", "bytes32"}
+	values = []interface{}{
+		"\x19Ethereum Signed Message:\n32",
+		hash,
+	}
+	raw_hash := solsha3.SoliditySHA3(types, values)
+
+	// sig, err := secp256k1.Sign(prefixed, math.PaddedBigBytes(key.D, 32))
+	sig, err := secp256k1.Sign(raw_hash, pkeyb)
+	if err != nil {
+		// panic(err)
+		return "", err
+	}
+	sig[64] += 27
+	return hex.EncodeToString(sig), nil
+}
+
+func signHash(data []byte) []byte {
+	msg := fmt.Sprintf("\x19Ethereum Signed Message:\n%d%s", len(data), data)
+	return crypto1.Keccak256([]byte(msg))
+}
+
+// func SigToPub(hash, sig []byte) (string, error) {
+// 	btcsig := make([]byte, 65)
+// 	btcsig[0] = sig[64] + 27
+// 	copy(btcsig[1:], sig)
+
+// 	pub, _, err := btcec.RecoverCompact(btcec.S256(), btcsig, hash)
+// 	ret := hex.EncodeToString(pub)
+// 	return ret, nil
+// }
+
+func Ecrecover(hash, sig []byte) ([]byte, error) {
+	return secp256k1.RecoverPubkey(hash, sig)
+}
+
+func TextAndHash(data []byte) ([]byte, string) {
+	msg := fmt.Sprintf("\x19Ethereum Signed Message:\n%d%s", len(data), string(data))
+	hasher := sha3.NewLegacyKeccak256()
+	hasher.Write([]byte(msg))
+	return hasher.Sum(nil), msg
+}
+
+func has0xPrefix(input string) bool {
+	return len(input) >= 2 && input[0] == '0' && (input[1] == 'x' || input[1] == 'X')
+}
+
+func (s *SignerService) RecoverPubkey(msg string, sig string) (string, error) {
+	s.logger.Printf("Process RecoverPubkey : %s, %s", msg, sig)
+
+	hexdata, _ := hexutil.Decode(msg)
+
+	hash, _ := TextAndHash(hexdata)
+
+	// raw_hash := crypto1.Keccak256(prefixed)
+
+	if len(sig) != 132 && !has0xPrefix(sig) {
+		// s.logger.Fatalln("sig length error : ", sig)
+		return "", errors.New("sig length error, must length  130 and 0x prefix")
+	}
+
+	hexsig, err := hex.DecodeString(sig[2:])
+	if err != nil {
+		// s.logger.Fatalln(err)
+		return "", err
+	}
+	hexsig[64] -= 27
+	// pksource, err := secp256k1.RecoverPubkey(signHash(raw_hash), hexsig);
+	// if err != nil {
+	// 	s.logger.Printf("Process RecoverPubkey error : %s, %s", msg, sig)
+	// 	return "", err;
+	// }
+	// address := crypto.PubkeyToAddress(pksource).Hex()
+	// pubkey, _ := crypto1.SigToPub(signHash(raw_hash), hexsig)
+	pubkey, _ := crypto1.SigToPub(hash, hexsig)
+
+	address := crypto1.PubkeyToAddress(*pubkey)
+	return address.String(), nil
 }
